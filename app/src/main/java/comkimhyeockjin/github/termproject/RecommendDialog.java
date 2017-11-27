e28d5ca0a2e7bf0c1e0e45b2f31d7ea02f7d2fba
Jihye Park <bgh0881@naver.com>
2017-11-27 21:01:59 +0900

    추천버튼 누르면 뜨는 dialog에서 TimePickerDialog 이제 뜸. theme 수정해야함.

--- app/src/main/java/comkimhyeockjin/github/termproject/RecommendDialog.java
+++ app/src/main/java/comkimhyeockjin/github/termproject/RecommendDialog.java
@@ -1,16 +1,20 @@
 package comkimhyeockjin.github.termproject;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 public class RecommendDialog extends Activity {
     public static final int RECOMMEND_REQUEST = 1;
+    int hour, min;
+    Button timeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +23,9 @@ public class RecommendDialog extends Activity {
         this.setTitle("어디로 갈까?");
 
         Calendar calendar = Calendar.getInstance();
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int min = calendar.get(Calendar.MINUTE);
-        Button timeBtn = (Button) findViewById(R.id.timeBtn);
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        min = calendar.get(Calendar.MINUTE);
+        timeBtn = (Button) findViewById(R.id.timeBtn);
         Log.d("TimePicker", (timeBtn==null)?"null":timeBtn.getText().toString());
         timeBtn.setText(hour+":"+min);
 
@@ -43,5 +47,18 @@ public class RecommendDialog extends Activity {
         intent.putExtra("personName", personName);
         intent.putExtra("time", time);
         startActivityForResult(intent, RECOMMEND_REQUEST);
+    }
+
+    public void timeBtnClick(View v) {
+        TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker v, int hh, int mm) {
+                //버튼에 설정한 시간 보여주기.
+                hour = hh; min = mm;
+                timeBtn.setText(hour+":"+min);
+            }
+        };
+        TimePickerDialog dialog = new TimePickerDialog(this,listener, hour, min, false);
+        dialog.show();
     }
 }
