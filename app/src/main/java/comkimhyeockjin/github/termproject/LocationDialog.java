4a342f7bbf8f9c35b39b5e436291852d66c6f314
Jihye Park <bgh0881@naver.com>
2017-11-27 22:45:37 +0900

    장소 추가를 위한 dialog를 위한 자바파일 생성.

---
+++ app/src/main/java/comkimhyeockjin/github/termproject/LocationDialog.java
@@ -0,0 +1,13 @@
+package comkimhyeockjin.github.termproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocationDialog extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_dialog);
+    }
+}
